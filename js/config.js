2a8624c1e852bf35a7c2620d02dd3f6c3b869eca
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-04-07 13:20:22 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var urlMapea = "http://mapea4-sigc.juntadeandalucia.es";
 var contextos = "wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Callejero,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Satélite";
 var layers = "";
-var urlGeosearch = "http://geobusquedas-sigc.juntadeandalucia.es/geobusquedas/sigc/search";
+var urlGeosearch = "";
 var extra = "";
-var urlServidor="http://192.168.61.63:8090";
-var urlMapa="http://192.168.61.63:3000";
+var urlServidor="http://www.callejerodeandalucia.es/pruebas";
+var urlMapa="http://www.callejerodeandalucia.es";
