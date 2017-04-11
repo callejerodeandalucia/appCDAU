fb05f9c58c2964517328226414192ff00b8be4f1
callejerodeandalucia <cdau.ieca@juntadeandalucia.es>
2017-04-11 11:38:47 +0200

    Update config.js
    Se añade URL geobúsquedas usada en el visualizador del portal de CDAU

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 var urlMapea = "http://mapea4-sigc.juntadeandalucia.es";
 var contextos = "wmcfile=http://www.callejerodeandalucia.es/wmc/context_cdau_callejero.xml*Callejero,http://www.callejerodeandalucia.es/wmc/context_cdau_satelite.xml*Satélite";
 var layers = "";
-var urlGeosearch = "";
+var urlGeosearch = "http://geobusquedas-sigc.juntadeandalucia.es/sigc/search?fq=-(keywords:via,plazas,avenida,carreteras,autovia,autopista)";
 var extra = "";
 var urlServidor="http://www.callejerodeandalucia.es/pruebas";
 var urlMapa="http://www.callejerodeandalucia.es";
